05435d3ab8873941c09d579e510f7cdbad102a31
Jevon Adelwin <jevonhura07gmail.com>
2022-06-29 20:58:01 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter'
+import CounterArrow from './components/CounterArrow';
+import CounterHook from './components/CounterHook';
+import ParentName from './ParentChild/ParentName';
+import ParentComponent from './ParentChild/ParentComponent';
+import EmployeeList from './List/EmployeeList';
+import ChartItem from './List/ChartItem';
+function App() {
+  return (
+    <div>
+      <ChartItem/>
+    </div>
+  );
+}
+
+export default App;
